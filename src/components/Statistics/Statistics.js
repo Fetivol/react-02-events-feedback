8fee801ddf46a962bc0d53e6f6c8146e7084d65d
Fetivol <Fetisowova@gmail.com>
2023-08-31 07:27:39 +0200

    Update Statistics.js

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -11,7 +11,7 @@ export const Statistics = ({
       <li>Neutral: {neutral}</li>
       <li>Bad: {bad}</li>
       <li>Total: {total}</li>
-      <li>Positive Feedback: {positivePercentage || 0} </li>
+      <li>Positive Feedback: {positivePercentage || 0} %</li>
     </ul>
   );
 };
